8469e5c78997683bd30032e72576777cec549b34
rafi-27 <147432818+rafi-27@users.noreply.github.com>
2024-09-25 17:56:58 +0200

    k

--- src/main/java/ies/thiar/Control/ControladorCliente.java
+++ src/main/java/ies/thiar/Control/ControladorCliente.java
@@ -1,14 +1,23 @@
 package ies.thiar.Control;
+import java.util.ArrayList;
 
 import ies.thiar.Modelo.Cliente;
+import ies.thiar.Modelo.LineaPedido;
 
 public class ControladorCliente {
     //ciienteActual
     private Cliente clienteActual;
+    private List<Cliente>listaClientes = new ArrayList<>();    
     //Illegal state exception
 
     //registrarCliente(--------) para agregar un cliente, Crear objeto , meter en tabla
-    
+    public void registrarCliente(Cliente cliente){
+
+    }
+
+
+
+
     //boolean loginCliente(correo, password) para logearse: buscar en tabla, check contrasenya si existe clienteActual = lo buscamos
 
     //despues de logearte usar clienteActual() va en login
