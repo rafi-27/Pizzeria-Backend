b81d777ca4902ff777f52d25b08c36949e2cf0b8
rafi-27 <147432818+rafi-27@users.noreply.github.com>
2024-10-16 11:05:08 +0200

    16/10

--- src/main/java/ies/thiar/Control/GestionFicheros.java
+++ src/main/java/ies/thiar/Control/GestionFicheros.java
@@ -1,5 +1,43 @@
 package ies.thiar.Control;
-
+import java.io.*;
 public class GestionFicheros {
     
+    public void gestionBasicaDeFicheros() throws IOException{
+        File file = new File("admin.txt"); 
+
+        if (file.exists()) { 
+            FileReader fr = new FileReader(file); 
+            BufferedReader bf = new BufferedReader(fr); 
+            String linea; 
+            
+            String[] primeraLinea = new String[0];
+            String[] segundaLinea = new String[0];
+            String[] terceraLinea = new String[0];
+
+            while ((linea = bf.readLine()) != null ){ 
+                System.out.println(linea.trim());
+                
+                
+
+
+
+
+
+
+
+            } 
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
 }
--- src/main/java/ies/thiar/MainPruebas.java
+++ src/main/java/ies/thiar/MainPruebas.java
@@ -1,7 +1,20 @@
 package ies.thiar;
 
+import java.io.IOException;
+
+import ies.thiar.Control.GestionFicheros;
+
 public class MainPruebas {
     public static void main(String[] args) {
+        GestionFicheros gestor = new GestionFicheros();
+        try {
+            gestor.gestionBasicaDeFicheros();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         
+
+
     }
 }
