ac5df58addd2a2db5fb326879e442062c17b3ef0
rafi-27 <147432818+rafi-27@users.noreply.github.com>
2024-11-30 17:25:29 +0100

    subir

--- src/main/java/ies/thiar/MainDePruebas.java
+++ src/main/java/ies/thiar/MainDePruebas.java
@@ -146,8 +146,6 @@ public class MainDePruebas {
 
             productUpdatear.setNombre("EjemploPrueba");
             
-
-
         } catch (Exception e) {
             e.getMessage();
             e.printStackTrace();
